d6ab8e4a1263ec2b109ac1e9073678875c53db61
w1iii <lomugdanglf.19@gmail.com>
2025-08-09 18:40:22 +0800

    add stuf below the carousel

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", (req,res) =>{
+    res.render("index");
+})
+
+app.listen(port, ()=>{
+    console.log(port);
+})
