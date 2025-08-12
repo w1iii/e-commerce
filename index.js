bd233ffc703e0ad8e798b1549d980fd37c24cf81
w1iii <lomugdanglf.19@gmail.com>
2025-08-12 13:14:11 +0800

    owner details added to viewitem(car). next add contact page
    contact page content:
    user will message to the admin if the user wants to put up his car for rent. email or contact, not sure what to use.

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
+
+
+// sample data base
+let rentalcars = [
+    {
+        "car_name": "Mitsubishi 3000GT",
+        "price": 0,
+        "ownder_details": {
+            "owner_name": "name",
+            "age": 0,
+            "address": "address",
+            "contact": 63 + "+",
+            "socials": [
+            ]
+        }
+        
+    }
+    // another car 
+]
+
+
 app.get("/", (req,res) =>{
     res.render("index");
 })
